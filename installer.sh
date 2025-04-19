cf62a147523f673c11b38222b43411dbfe26b66c
Sid. <burhanverse@gmail.com>
2025-04-19 12:40:16 +0000

    rewriten script to be compatible with both debian and termux

--- installer.sh
+++ installer.sh
@@ -1,9 +1,58 @@
-#!/data/data/com.termux/files/usr/bin/bash
-pkg install -y root-repo 
-pkg install -y git tsu python wpa-supplicant pixiewps iw
+#!/bin/bash
 
+# Detect if running on Termux or Debian
+if [ -d "/data/data/com.termux" ]; then
+    OS="termux"
+    PKG_MANAGER="pkg"
+else
+    OS="debian"
+    PKG_MANAGER="apt"
+fi
+
+# Function to install packages
+install_packages() {
+    if [ "$OS" = "termux" ]; then
+        # Termux-specific package installation
+        $PKG_MANAGER update
+        $PKG_MANAGER install -y root-repo
+        $PKG_MANAGER install -y git tsu python wpa-supplicant pixiewps iw
+    else
+        # Debian-specific package installation
+        $PKG_MANAGER update
+        $PKG_MANAGER install -y git sudo python3 wpasupplicant pixiewps iw
+    fi
+}
+
+# Function to determine Python command
+get_python_cmd() {
+    if [ "$OS" = "termux" ]; then
+        echo "python"
+    else
+        echo "python3"
+    fi
+}
+
+# Install required packages
+install_packages
+
+# Clone the OneShot repository
 git clone --depth 1 https://github.com/Burhanverse/OneShot OneShot
 
+# Set executable permissions
 chmod +x OneShot/oneshot.py
 
-printf "###############################################\n#  All done! Now you can run OneShot with\n#   sudo python OneShot/oneshot.py -i wlan0 -K\n#\n#  To update, run\n#   (cd OneShot && git pull)\n###############################################\n"
+# Get the appropriate Python command
+PYTHON_CMD=$(get_python_cmd)
+
+# Print usage instructions
+printf "###############################################\n"
+printf "#  All done! Now you can run OneShot with:\n"
+if [ "$OS" = "termux" ]; then
+    printf "#   tsu -c \"$PYTHON_CMD OneShot/oneshot.py -i wlan0 -K\"\n"
+else
+    printf "#   sudo $PYTHON_CMD OneShot/oneshot.py -i wlan0 -K\n"
+fi
+printf "#\n"
+printf "#  To update, run:\n"
+printf "#   (cd OneShot && git pull)\n"
+printf "###############################################\n"
